3c11409b41090e26f1d892d747afa5adf9bbc2d5
Sebastian Georgi <sebastian@georgi.io>
2022-03-02 13:11:02 +0100

    Pulumi Roles

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -3,8 +3,41 @@ const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
 const awsx = require("@pulumi/awsx");
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const IDENTITY_PROVIDER_ARN = 'arn:aws:iam::927485958639:oidc-provider/token.actions.githubusercontent.com';
 
-// Export the name of the bucket
-exports.bucketName = bucket.id;
+let deployRole = new aws.iam.Role('deploy-role', {
+  assumeRolePolicy: JSON.stringify({
+    Version: '2012-10-17',
+    Statement: [{
+      Sid: 'RoleForGithub',
+      Action: 'sts:AssumeRoleWithWebIdentity',
+      Effect: 'Allow',
+      Principal: {Federated: IDENTITY_PROVIDER_ARN},
+      Condition: {StringLike: {'token.actions.githubusercontent.com:sub': `repo:georgi-io/test-frontend:*`}}
+    }]
+  })
+});
+
+let deployRolePolicy = new aws.iam.RolePolicy('deploy-role-policy', {
+  role: deployRole.id,
+  policy: JSON.stringify({
+    Version: '2012-10-17',
+    Statement: [{
+      Action: [
+        'ecr:BatchGetImage',
+        'ecr:BatchCheckLayerAvailability',
+        'ecr:CompleteLayerUpload',
+        'ecr:GetDownloadUrlForLayer',
+        'ecr:InitiateLayerUpload',
+        'ecr:PutImage',
+        'ecr:UploadLayerPart',
+        'ecr:GetAuthorizationToken'
+      ],
+      Effect: 'Allow',
+      Resource: '*'
+    }]
+  })
+});
+
+// Needs to be put into GitHub-Action
+exports.deployRoleARN = deployRole.arn;
